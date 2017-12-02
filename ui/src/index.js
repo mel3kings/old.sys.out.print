aa57060eb1b06f79813775656bb825c5891d37e2
Melchor Tatlonghari <Melchor.Tatlonghari@riskflo.com.au>
2017-12-03 00:50:36 +1100

    Gulp + React + Boostrap server setup

---
+++ ui/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>React simple starter</div>
+        );
+    }
+}
+
+ReactDOM.render(
+  <App/>
+    , document.querySelector('.react'));
