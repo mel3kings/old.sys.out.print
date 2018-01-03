8b598dc3ca8d445ba90d03e9c11f80df4d3972dc
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-01-03 21:07:32 +1100

    Fixed ads, added SEO optimizations

--- ui/components/Technologies/content_page.js
+++ ui/components/Technologies/content_page.js
@@ -2,18 +2,33 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {renderTechPage, renderTechListPage, renderContentPage} from "../../actions/action_render_page";
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-
+import DocumentMeta from 'react-document-meta';
 class ContentPage extends React.Component {
     componentWillMount() {
         this.props.renderContentPage(this.props.match.params.type, this.props.match.params.id);
     }
 
     render() {
+        const meta = {
+            title: `${this.props.tech.header}`,
+            description:`how to ${this.props.tech.header} tutorial`,
+            meta: {
+                charset: 'utf-8',
+                name: {
+                    keywords: `${this.props.match.params.type}, ${this.props.tech.header}, tutorial, how to, java, aws, docker`
+                }
+            }
+        };
         return <div className="container">
-            <h2>{this.props.tech.header}</h2>
+            <DocumentMeta {...meta} />
+            <h2>{this.props.tech.header} ASD</h2>
              {ReactHtmlParser(this.props.tech.data)}
         </div>
     }
+    componentDidMount(){
+        document.title=this.props.tech.header;
+    }
+
 }
 
 function mapStateToProps(state) {
