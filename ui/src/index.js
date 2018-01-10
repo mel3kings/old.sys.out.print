1f2e5c7560eb44f4154b41e2f83d2a73954e14eb
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-01-10 17:11:58 +1100

    added new logo and favicon

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Home from '../components/Technologies/home';
 import Showcase from '../components/showcase';
@@ -6,10 +6,11 @@ import TechnologyList from '../components/Technologies/technology_list';
 import ContentPage from '../components/Technologies/content_page';
 import TechnologiesBoxes from '../components/technologies_boxes';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import promise from 'redux-promise';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import reducers from '../reducers';
+import Favicon from 'react-favicon';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,23 +20,25 @@ class App extends React.Component {
             <div>
                 <Showcase/>
                 <TechnologiesBoxes/>
-
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/technologies/:type/:id" component={ContentPage}/>
-                    <Route path="/technologies/:type" component={TechnologyList}/>
-                    <Route path="/technologies" component={Home} />
-                    <Route path="/" component={App} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    </Provider>
+    <div>
+        <Favicon url="favicon.ico"/>
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route path="/technologies/:type/:id" component={ContentPage}/>
+                        <Route path="/technologies/:type" component={TechnologyList}/>
+                        <Route path="/technologies" component={Home}/>
+                        <Route path="/" component={App}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </Provider>
+    </div>
     , document.querySelector('.react'));
