0cf13d8ba4fca1c2dea076570b6b0405db241996
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-04-07 11:03:01 +1000

    updated home page

--- ui/components/technologies_boxes.js
+++ ui/components/technologies_boxes.js
@@ -12,7 +12,9 @@ export default class TechnologiesBoxes extends React.Component{
                             <h1>New Updates: April 2018</h1>
                             <p class="d-none d-md-block">
                                 <ul class="list-group">
-                                    <li class="list-group-item"><a href="/technologies/infrastructure/69"><b>Terraform - Infrastructure through code</b></a></li>
+                                    <li class="list-group-item">
+                                        <img src="../../src/img/icon/terra.jpg" type="image/jpg" height={25} width={25}/>
+                                        <a href="/technologies/infrastructure/69"><b>Terraform - Infrastructure through code</b></a></li>
                                 </ul>
                             </p>
                         </div>
@@ -20,12 +22,24 @@ export default class TechnologiesBoxes extends React.Component{
                             <h1>New Updates: March 2018</h1>
                             <p class="d-none d-md-block">
                                 <ul class="list-group">
-                                    <li class="list-group-item"><a href="/technologies/python">New Language - Python</a></li>
-                                    <li class="list-group-item"><a href="/technologies/algorithms/61">Data Structures - Queue</a></li>
-                                    <li class="list-group-item"><a href="/technologies/algorithms/60">Data Structures - Stack</a></li>
-                                    <li class="list-group-item"><a href="/technologies/algorithms/59">Big O Notation - Revisited</a></li>
-                                    <li class="list-group-item"><a href="/technologies/big-data/58">Kafka Multiple Brokers Config</a></li>
-                                    <li class="list-group-item"><a href="/technologies/tools/57">Creating Local CI/CD Using Jenkins Server (Docker + AWS)</a></li>
+                                    <li class="list-group-item">
+                                        <img src="../../src/img/icon/python.png" type="image/png" height={25} width={25}/>
+                                        <a href="/technologies/python">New Language - Python</a></li>
+                                    <li class="list-group-item"><a href="/technologies/algorithms/61">
+                                        <img src="../../src/img/logos/algorithm.png" type="image/png" height={25} width={25}/>
+                                        Data Structures - Queue</a></li>
+                                    <li class="list-group-item"><a href="/technologies/algorithms/60">
+                                        <img src="../../src/img/logos/algorithm.png" type="image/png" height={25} width={25}/>
+                                        Data Structures - Stack</a></li>
+                                    <li class="list-group-item"><a href="/technologies/algorithms/59">
+                                        <img src="../../src/img/logos/algorithm.png" type="image/png" height={25} width={25}/>
+                                        Big O Notation - Revisited</a></li>
+                                    <li class="list-group-item"><a href="/technologies/big-data/58">
+                                        <img src="../../src/img/icon/kafka.png" type="image/png" height={25} width={25}/>
+                                        Kafka Multiple Brokers Config</a></li>
+                                    <li class="list-group-item"><a href="/technologies/tools/57">
+                                        <img src="../../src/img/logos/tools.png" type="image/png" height={25} width={25}/>
+                                        Creating Local CI/CD Using Jenkins Server (Docker + AWS)</a></li>
                                     <li class="list-group-item"><a href="/architectures">New Architecture (Kafka + Grails) > Architectures page</a></li>
                                 </ul>
                             </p>
@@ -34,19 +48,68 @@ export default class TechnologiesBoxes extends React.Component{
                             <h1>New Updates: February 2018</h1>
                             <p class="d-none d-md-block">
                                 <ul class="list-group">
-                                    <li class="list-group-item">Implemented Continuous Integration/Deployment: Upcoming Jenkins Tutorial</li>
-                                    <li class="list-group-item">Updated NO-SQL to Big-Data</li>
-                                    <li class="list-group-item">Apache Kafka (Big-Data)</li>
-                                    <li class="list-group-item">How to Install Docker on EC2 (Docker)</li>
-                                    <li class="list-group-item">Tools Page</li>
-                                    <li class="list-group-item">Using NGROK (Tools) </li>
-                                    <li class="list-group-item">Kubernetes Basics</li>
-                                    <li class="list-group-item">Throttling + Java Atomic Library</li>
-                                    <li class="list-group-item">Grails (Groovy on Grails)</li>
-                                    <li class="list-group-item">Architectures Tab</li>
-                                    <li class="list-group-item">Eulers Challenges under Algorithm</li>
-                                    <li class="list-group-item">LightSail > GoDaddy/Route 53 Setup</li>
-                                    <li class="list-group-item">Running a React app on Docker</li>
+                                    <li class="list-group-item">
+                                        <img src="../../src/img/icon/kafka.png" type="image/png" height={25} width={25}/>
+                                        <a href="/technologies/big-data/54"> Apache Kafka (Big-Data)</a></li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/docker/55">
+                                            <img src="../../src/img/logos/docker.png" type="image/png" height={25} width={25}/>
+                                        How to Install Docker on EC2
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/tools">
+                                            <img src="../../src/img/logos/tools.png" type="image/png" height={25} width={25}/>
+                                            Tools Page
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/tools">
+                                        <img src="../../src/img/logos/tools.png" type="image/png" height={25} width={25}/>
+                                            Using NGROK
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/infrastructure/52">
+                                        <img src="../../src/img/logos/kubernetes.png" type="image/png" height={25} width={25}/>
+                                            Kubernetes Basics
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/java/51">
+                                        <img src="../../src/img/logos/java.png" type="image/png" height={25} width={25}/>
+                                        Throttling + Java Atomic Library
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/grails/53">
+                                        <img src="../../src/img/logos/grails.png" type="image/png" height={25} width={25}/>
+                                        Grails (Groovy on Grails)
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/architecture">
+                                        Architectures Tab
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/algorithms/47">
+                                        <img src="../../src/img/logos/algorithm.png" type="image/png" height={25} width={25}/>
+                                        Eulers Challenges under Algorithm
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/aws/50">
+                                        <img src="../../src/img/logos/aws.png" type="image/png" height={25} width={25}/>
+                                        LightSail > GoDaddy/Route 53 Setup
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="/technologies/docker/45">
+                                        <img src="../../src/img/logos/docker.png" type="image/png" height={25} width={25}/>
+                                        Running a React app on Docker
+                                        </a>
+                                    </li>
                                 </ul>
                             </p>
                         </div>
@@ -60,14 +123,17 @@ export default class TechnologiesBoxes extends React.Component{
                         <AdSense.Google
                             client='ca-pub-9837645739197927'
                             slot='9279171029'
+                            style={{ width: 300, height: 500, float: 'left' }}
                         />
                         <AdSense.Google
                             client='ca-pub-9837645739197927'
                             slot='5092747452'
+                            style={{ width: 300, height: 500, float: 'left' }}
                         />
                         <AdSense.Google
                             client='ca-pub-9837645739197927'
                             slot='7685322363'
+                            style={{ width: 300, height: 500, float: 'left' }}
                         />
 
                     </div>
