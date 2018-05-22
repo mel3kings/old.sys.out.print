5e78ee693ab26fac0e42e602fa96c1b6e37b439d
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-05-22 21:21:10 +1000

    node test middleware + templating

--- algorithm/src/main/node/HelloWorld.js
+++ algorithm/src/main/node/HelloWorld.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res, next){
+   console.log("interceptor");
+   next();
+});
+
 app.get("/", function(req, res){
-    res.send('<html><head></head><body>Hello world</body></html>')
+    res.render('index', {ID: '1234'});
+
 });
 app.listen(port);
