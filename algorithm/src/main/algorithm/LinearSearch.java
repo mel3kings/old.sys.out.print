d2a3a2e42cd7fcb586c70cf236ec1a70e47e12d4
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2018-05-27 17:51:09 +1000

    Recursive Binary + Linear

--- algorithm/src/main/algorithm/LinearSearch.java
+++ algorithm/src/main/algorithm/LinearSearch.java
@@ -5,7 +5,7 @@ public class LinearSearch {
     public static void main(String[] args){
         int[] a = {3,5,79,13,789,12,6,8};
         int x = 12;
-        System.out.println(search(a,x));
+        System.out.println(recursiveSearch(a,x, 0));
     }
 
     public static int search(int[] a, int x){
@@ -15,5 +15,17 @@ public class LinearSearch {
             }
         }
         return -1;
+    }
+
+    public static int recursiveSearch(int[] a, int x, int i){
+        if(i > a.length){
+            return -1;
+        } else {
+            if(a[i] == x){
+                return i;
+            } else {
+              return recursiveSearch(a, x, ++i);
+            }
+        }
     }
 }
